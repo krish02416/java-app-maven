07c5e9fee90b407bbc4d119892274b9072676cf7
krish02416 <84865439+krish02416@users.noreply.github.com>
2026-01-17 23:55:02 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,9 +5,9 @@ def buildJar(){
 def buildImage(){
     echo "Building the docker image...."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable:'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t harikrishnan20010616/demo-app:jma-2.0 .'
+        sh 'docker build -t harikrishnan20010616/demo-app:1.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push harikrishnan20010616/demo-app:jma-2.0'
+        sh 'docker push harikrishnan20010616/demo-app:1.0'
     }
 }
 def deployApp(){
